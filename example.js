c254bd578dd43f3edd716bb8db9d35066eac2803
kumarroyjayanta <kumarroyjayanta@gmail.com>
2020-07-22 16:27:48 +0530

    filter function introduced

--- example.js
+++ example.js
@@ -7,3 +7,35 @@ var waffle = triple
 console.log(waffle(30))
 
 //function can also be supplied to highed order functions 
+
+var animals = [
+    {name:'Fluffykins' ,species:'rabbit'},
+    {name:'caro',species:'dog'},
+    {name:'Hamilton',species:'dog'},
+    {name:'harold',species:'fish'},
+    {name:'ursula',species:'cat'},
+    {name:'Jimmy',species:'fish'},
+]
+
+// var dogs=[]
+// for(var i = 0;i<animals.length;i++){
+//     if(animals[i].species==='dog'){
+//         dogs.push(animals[i])
+//     }
+// } 
+// SAME THING CAN BE DONE BY FILTER FUNCTION
+
+// var dog = animals.filter(function(animal){
+//     return animal.species==='dog'
+// })
+
+// YET ANOTHER WAY IS
+
+var isDog = function(animal){
+    return animal.species ==='dog'
+}
+
+var dog = animals.filter(isDog)
+
+
+console.log(dog)
