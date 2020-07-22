6f36f67bff9ed1b4414953459f4c5aa333667099
kumarroyjayanta <kumarroyjayanta@gmail.com>
2020-07-22 15:06:36 +0530

    example.js added

---
+++ example.js
@@ -0,0 +1,9 @@
+var triple = function(x){
+    return x*3;
+}
+// you can assign function just like variable
+var waffle = triple
+
+console.log(waffle(30))
+
+//function can also be supplied to highed order functions 
